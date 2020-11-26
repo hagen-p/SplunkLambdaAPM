04fdcb5ed4d032b9eb09e2c5d0146a227ed4f663
p-hagen <hagenpcjm@gmailcom>
2020-11-26 11:35:37 +0100

    synched order line

--- Lambdas/APM/RetailOrderLineAPM/Lambda_Function.py
+++ Lambdas/APM/RetailOrderLineAPM/Lambda_Function.py
@@ -13,19 +13,16 @@ LAMBDA_FUNCTION = os.environ['AWS_LAMBDA_FUNCTION_NAME']
 @signalfx_lambda.is_traced(with_span=False)
 def lambda_handler(event, context):
     # Setup tracer so we can create span and set the B3 Headers
-    print ('event:')
-    print (event)
+    print ("event: ", event)
     tracer = opentracing.tracer
     TraceHeaders =  event ['TraceHeaders']  # Value passed in from  calling app
-    print ('TraceHeaders:')
-    print (TraceHeaders)
+    print ("TraceHeaders: ", TraceHeaders)
     span_ctx = tracer.extract(Format.HTTP_HEADERS, TraceHeaders)
-    print("span_ctx:")
-    print (span_ctx)
+    print("span_ctx: ", span_ctx)
     span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER, "environment" : APM_ENVIRONMENT}
     with tracer.start_active_span(LAMBDA_FUNCTION , child_of=span_ctx, tags=span_tags):
         span = tracer.active_span #grabing the active span for Custom Tags
-        print (span)
+        print ("Span: ", span)
         with tracer.start_active_span(LAMBDA_FUNCTION+"_input", tags=span_tags) as scope:
             scopespan = scope.span
         
