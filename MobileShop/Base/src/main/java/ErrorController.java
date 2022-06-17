0944160a07f7f55a36d6a04028ecdee7df614e30
p-hagen-sfx <phagen@signalfx.com>
2022-06-17 17:16:36 +0200

    remove of errorController Class

--- MobileShop/Base/src/main/java/ErrorController.java
+++
@@ -1,9 +0,0 @@
-@Controller
-public class MyErrorController implements ErrorController  {
-
-    @RequestMapping("/error")
-    public String handleError() {
-        //do something like logging
-        return "error";
-    }
-}
